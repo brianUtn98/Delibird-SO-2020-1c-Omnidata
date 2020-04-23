9cceb9964b38109ce1692e4a55df0a210016a97d
emarcosgv <emarcosgv@gmail.com>
2020-04-23 20:17:27 -0300

    inicio de gameBoy

--- gameBoy/src/funcionesGameBoy.c
+++ gameBoy/src/funcionesGameBoy.c
@@ -1,2 +1,100 @@
 #include "gameBoy.h"
 
+void inicializarLogger() {
+	//Crea el logger
+	logger = log_create("logs/GAMEBOY.log", "GAMEBOY", 1, LOG_LEVEL_TRACE);
+}
+
+void cargarConfigGameBoy() {
+	//printf("pude cargar la configuracion correctamente");
+	//Carga la configuracion del txt de config al struct de config
+
+	GAMEBOYTConfig = config_create(GAMEBOY_CONFIG_PATH);
+	//inicializarLogger();
+	if (GAMEBOYTConfig == NULL) {
+		perror("error archivo de configuracion");
+		log_error(logger, "- NO SE PUDO IMPORTAR LA CONFIGURACION");
+		exit(1);
+	}
+
+	log_info(logger, "- CONFIGURACION IMPORTADA\n");
+
+	gameBoyConf = malloc(sizeof(t_GAMEBOYConfig));
+
+	gameBoyConf->ipBroker = config_get_string_value(GAMEBOYTConfig,
+			"IP_BROKER");
+	gameBoyConf->ipTeam = config_get_string_value(GAMEBOYTConfig, "IP_TEAM");
+	gameBoyConf->ipGameCard = config_get_string_value(GAMEBOYTConfig,
+			"IP_GAMECARD");
+
+	gameBoyConf->puertoBroker = config_get_int_value(GAMEBOYTConfig,
+			"PUERTO_BROKER");
+	gameBoyConf->puertoTeam = config_get_int_value(GAMEBOYTConfig,
+			"PUERTO_TEAM");
+	gameBoyConf->puertoGameCard = config_get_int_value(GAMEBOYTConfig,
+			"PUERTO_GAMECARD");
+
+	//brokerConf->logFile = config_get_string_value(GAMEBOYTConfig, "LOG_FILE");
+
+	printf(" puerto broker usado: %d \n", gameBoyConf->puertoBroker);
+	printf(" Puerto team usado: %d \n", gameBoyConf->puertoTeam);
+	printf(" ip broker usado: %s \n", gameBoyConf->ipBroker);
+
+	log_info(logger, "· Puerto escucha = %d", gameBoyConf->puertoBroker);
+	log_info(logger, "· IP  = %s", gameBoyConf->ipBroker);
+
+	config_destroy(GAMEBOYTConfig);
+	//ver cuando liberar el brokerConf , si lo hacemos acá no se va a poder usar en el servidor por ej,
+	//estariamos cargando una estructura y liberandola sin darle uso.
+	// talvez podriamos devolver la estructura y que la función no sea void.
+	//hacer...
+	//return brokerConf;
+	//free(brokerConf);
+}
+//Hago esta función para acordarnos que debemos liberar el malloc de Sofi
+void liberarBrokerConf() {
+	free(gameBoyConf);
+}
+
+void manejarMensajes(int argc, char *argv[]) {
+
+
+
+	switch (strcmp(argv[1],"BROKER")) {
+		case tBrokerNewPokemon: {
+			break;
+		}
+		case tBrokerAppearedPokemon: {
+			break;
+		}
+		case tBrokerCatchPokemon: {
+			break;
+		}
+		case tBrokerCaughtPokemon: {
+			break;
+		}
+		case tBrokerGetPokemon: {
+			break;
+		}
+		case tTeamAppearedPokemon: {
+			break;
+		}
+		case tGameCardNewPokemon: {
+			break;
+		}
+		case tGameCardCatchPokemon: {
+			break;
+		}
+		case tGameCardGetPokemon: {
+			break;
+		}
+		case tModoSuscriptor: {
+			break;
+		}
+		default: {
+			printf("Mensaje invalido, ingrese escriba nuevamente.Gracias.");
+			break;
+		}
+		}
+
+	}
--- gameBoy/src/gameBoy.h
+++ gameBoy/src/gameBoy.h
@@ -3,5 +3,52 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <commons/log.h>
+#include <commons/config.h>
+#include <commons/collections/list.h>
+#include <commons/collections/queue.h>
+//#include "../sockets/servidor.h"
+
+#define GAMEBOY_CONFIG_PATH "gameBoy.config"
+
+typedef enum t_protocolo {
+	tBrokerNewPokemon = 1,
+	tBrokerAppearedPokemon,
+	tBrokerCatchPokemon,
+	tBrokerCaughtPokemon,
+	tBrokerGetPokemon,
+	tTeamAppearedPokemon,
+	tGameCardNewPokemon,
+	tGameCardCatchPokemon,
+	tGameCardGetPokemon,
+	tModoSuscriptor,
+	tFinDeProtocolo //NO SACAR Y DEJAR A LO ULTIMO!!!
+} t_protocolo;
+
+
+typedef struct {
+	char *ipBroker;
+	char *ipTeam;
+	char *ipGameCard;
+	int puertoBroker;
+	int puertoTeam;
+	int puertoGameCard;
+
+} t_GAMEBOYConfig;
+
+
+t_log *logger;
+t_config *GAMEBOYTConfig;
+t_GAMEBOYConfig *gameBoyConf;
+
+
+void inicializarLogger(void);
+void cargarConfigGameBoy(void);
+void iniciarServidor(void);
+void manejarMensajes(int argc,char *argv[]);
+
+
+
+
 
 #endif /* GAME_BOY_GAMEBOY_H_ */
