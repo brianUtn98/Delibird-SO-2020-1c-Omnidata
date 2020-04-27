e81d8d7fc13792301b26ddc19b56a46b8fe02626
brianUtn98 <brian.gmonroy98@gmail.com>
2020-04-27 01:19:35 -0300

    Probando el branc...

--- team/src/team.c
+++ team/src/team.c
@@ -4,7 +4,9 @@ int main(void) {
 
 iniciarlizar_loggerTeam();
 cargarConfigTeam();
-char *pruebita="Linea para probar git";
+char *pruebita="Linea para probar git\n";
+char *otraPruebita="Linea para probar branch\n";
 printf("%s",pruebita);
+printf("%s",otraPruebita);
 	return EXIT_SUCCESS;
 }
