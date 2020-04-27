f51b2e054205303366fe6d680b8c76c3ee925ce8
brianUtn98 <brian.gmonroy98@gmail.com>
2020-04-27 01:12:28 -0300

    Limpie codigo en el proceso team

--- team/src/funcionesTeam.c
+++ team/src/funcionesTeam.c
@@ -37,44 +37,14 @@ void cargarConfigTeam() {
 			"PUERTO_BROKER");
 	teamConf->LOG_FILE = config_get_string_value(TEAMTConfig, "LOG_FILE");
 	log_info(logger, "- CONFIGURACION IMPORTADA\n");
+
 //	t_list *string = list_create();
 //		char**	pokes = string_split(string, '|');
 //	printf("%s",string);
 //
 //	string_split(teamConf->POKEMON_ENTRENADOR, '|');
 //	string_split(teamConf->OBJETIVOS_ENTRENADOR, '|');
+
 	config_destroy(TEAMTConfig);
-	//free(teamConf);
 return;
 }
-
-//void main(void) {
-//  t_config *config = config_create("./tp_fix.config");
-//  char **read_array = config_get_array_value(config, "POKEMON_ENTRENADORES");
-//  t_list *pokemongos = list_create();
-//  void _a_la_lista(char *poke) {
-//    if (poke != NULL) {
-//      list_add(pokemongos, poke);
-//    }
-//  }
-//  void _imprimir(char *string) {
-//    if(string != NULL) {
-//      char **pokes = string_split(string, "|");
-//      string_iterate_lines(pokes, _a_la_lista);
-//      printf("Read: %s\n", string);
-//    } else {
-//      printf("Got NULL\n");
-//    }
-//  }
-//  string_iterate_lines(read_array, _imprimir);
-//
-//  void mostrar(void *elemento) {
-//    printf("El elemento: %s\n", (char *)elemento);
-//  }
-//
-//  list_iterate(pokemongos, mostrar);
-//
-//  // Atencion: aca les falta liberar la memoria que haga falta
-//  // Es cuarentena; no Navidad
-//}
-
--- team/src/team.c
+++ team/src/team.c
@@ -6,36 +6,5 @@ iniciarlizar_loggerTeam();
 cargarConfigTeam();
 char *pruebita="Linea para probar git";
 printf("%s",pruebita);
-//	//crear_conexion(teamConf->IP_BROKER, teamConf->PUERTO_BROKER);
-
-//int main(void) {
-//	//t_config *config = config_create("./tp_fix.config");
-//	char **read_array = teamConf->POKEMON_ENTRENADOR;//config_get_array_value(config, "POKEMON_ENTRENADORES");
-//	t_list *pokemongos = list_create();
-//	void _a_la_lista(char *poke) {
-//		if (poke != NULL) {
-//			list_add(pokemongos, poke);
-//		}
-//	}
-//	void _imprimir(char *string) {
-//		if (string != NULL) {
-//			char **pokes = string_split(string, "|");
-//			string_iterate_lines(pokes, _a_la_lista);
-//			printf("Read: %s\n", string);
-//		} else {
-//			printf("Got NULL\n");
-//		}
-//	}
-//	string_iterate_lines(read_array, _imprimir);
-//
-//	void mostrar(void *elemento) {
-//		printf("El elemento: %s\n", (char *) elemento);
-//	}
-//
-//	list_iterate(pokemongos, mostrar);
-//
-//	// Atencion: aca les falta liberar la memoria que haga falta
-//	// Es cuarentena; no Navidad
-
 	return EXIT_SUCCESS;
 }
