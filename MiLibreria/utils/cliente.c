ddfa6720a5d5e2dddec35401ba8c37e6b0bad64f
Laura Sofia Enriquez <lsofia.enriquez@gmail.com>
2020-04-29 23:00:34 -0300

    Sistema de reintento de conexion con el broker
    En caso que la conexion no llegue a realizarse o se caiga, el proceso Team
    debera contar con un sistema de reintento de conexion cada X segundos
    configurado desde archivo de configuracion.
    
    TRABAJO FALTANTE
    Falta pasar los tiempos desde los archivos de configuarion en vez de hardcodearlos
    como lo estoy haciendo.

--- MiLibreria/utils/cliente.c
+++ MiLibreria/utils/cliente.c
@@ -1,16 +1,30 @@
 #include "cliente.h"
 #include "utils.h"
 void conectarseAServidor(char *IP,int PUERTO) {
+	//HAY QUE PASAR ESTO POR PARAMETROS
+	int tiempo_reconexion=30;
+	int max_intentos=3;
+	int retry=0;
+
 	struct sockaddr_in dirServer;
 	dirServer.sin_family= AF_INET;
 	dirServer.sin_addr.s_addr=inet_addr("127.0.0.1");
 	dirServer.sin_port=htons(PUERTO);
 
 	int cliente=socket(AF_INET,SOCK_STREAM,0);
-	if(connect(cliente, (void*)&dirServer,sizeof(dirServer)) != 0){
-		perror("No se pudo conectar");
-		exit(1);
-	}
+	do{
+		if(connect(cliente, (void*)&dirServer,sizeof(dirServer)) != 0){
+			retry = retry + 1;
+			if(retry==max_intentos){
+				perror("Se supero la cantidad de intentos posibles");
+				exit(1);
+			}
+			perror("No se pudo conectar");
+			sleep(tiempo_reconexion);
+		}
+
+	}while(retry<max_intentos);
+
 	printf("Se pudo conectar al servidor");
 	while(1){
 	char mensaje[1000];
