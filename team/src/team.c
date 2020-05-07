6e908c31f2f6f6d416aa42bca6bac977c5e2bfd4
brianUtn98 <brian.gmonroy98@gmail.com>
2020-05-06 23:47:01 -0300

    Agregué creación de entrenadores, está en versión prueba

--- team/src/team.c
+++ team/src/team.c
@@ -7,11 +7,21 @@ int main(void) {
 
 inicializarLoggerTeam();
 cargarConfigTeam();
+for(int i=0;i<cantidadEntrenadores;i++){
+log_info(logger,"------ Entrenador %d ------",i+1);
+log_info(logger,"------Posicion: X=%d,Y=%d ------",entrenadores[i].posicion.x,entrenadores[i].posicion.y);
+log_info(logger,"------- Los pokemons del entrenador %d son: ",i+1);
+mostrarLista(entrenadores[i].pokemons);
+log_info(logger,"------ Los objetivos del entrenador %d son: ",i+1);
+mostrarLista(entrenadores[i].objetivos);
+}
 //crearEntrenadores();
 // 1. Crear conexion
 int socketCliente;
 log_info(logger,"Conectando a PUERTO=%d en IP=%s",teamConf->PUERTO_BROKER,teamConf->IP_BROKER);
 socketCliente=crearConexion(teamConf->IP_BROKER,teamConf->PUERTO_BROKER,teamConf->TIEMPO_RECONEXION);
+
+
 
 // 2. Suscribirse a las colas del Broker
 
